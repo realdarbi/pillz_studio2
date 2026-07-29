9af42431b820107ebc7452afb321c25f8d768b6a
realdarbi <kkartofun@gmail.com>
2026-07-29 15:20:44 +0700

    15:21 29.07

--- build_files.sh
+++ build_files.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
-echo "Installing dependencies..."
-
-# Создаём виртуальное окружение
-python3 -m venv venv
-
-# Активируем его
-source venv/bin/activate
-
-# Устанавливаем зависимости
+echo "Устанавливаем зависимости..."
 pip install --upgrade pip
 pip install -r requirements.txt
 
-echo "Collecting static files..."
+echo "Собираем статику..."
 python manage.py collectstatic --noinput
 
-echo "Running migrations..."
-python manage.py migrate --noinput
+# Миграции не запускаем — они накатятся при старте приложения
