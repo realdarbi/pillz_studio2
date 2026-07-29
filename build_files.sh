16709f89dec8c3657884e0b0d44bc9011b775606
realdarbi <kkartofun@gmail.com>
2026-07-29 15:27:26 +0700

    15:28 29.07

--- build_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Устанавливаем зависимости..."
-pip install --upgrade pip
-pip install -r requirements.txt
-
-echo "Собираем статику..."
-python manage.py collectstatic --noinput
-
-# Миграции не запускаем — они накатятся при старте приложения
